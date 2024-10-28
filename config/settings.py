1278057bbb9cc7d222f88dd508644b3169effbe4
ystass <stepanischevyy@mail.ru>
2024-10-28 16:53:52 +0400

    использование модели User для авторизации

--- config/settings.py
+++ config/settings.py
@@ -132,6 +132,8 @@ STATIC_URL = "static/"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+AUTH_USER_MODEL = "users.User"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
