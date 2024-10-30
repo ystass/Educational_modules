37d4de2b4d1af1ef6149d83b0a4fae5bc6d19c5d
ystass <stepanischevyy@mail.ru>
2024-10-30 15:23:42 +0400

    создание моделей Module и Lesson

--- modules/models.py
+++ modules/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+from config import settings
+
+NULLABLE = {"null": True, "blank": True}
+
+
+class Module(models.Model):
+    serial_number = models.PositiveIntegerField(verbose_name="Порядковый номер учебного модуля")
+    title = models.CharField(max_length=100, verbose_name="Название учебного модуля")
+
+    description = models.TextField(verbose_name="Описание модуля", **NULLABLE)
+
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name="Создатель учебного модуля",
+        **NULLABLE
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    title = models.CharField(
+        max_length=100, verbose_name="Название урока", help_text="Укажите название урока"
+    )
+
+    description = models.TextField(verbose_name="Описание урока", **NULLABLE)
+
+    preview = models.ImageField(upload_to="materials/lessons", **NULLABLE)
+
+    url = models.URLField(verbose_name="Ccылка на видео", **NULLABLE)
+
+    course = models.ForeignKey(
+        Module, on_delete=models.SET_NULL, verbose_name="Курс", **NULLABLE
+    )
+
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name="Создатель урока",
+        **NULLABLE
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
