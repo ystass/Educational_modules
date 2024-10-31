eb43aa1812a100fde168f715cebfd795a3e508ef
ystass <stepanischevyy@mail.ru>
2024-10-31 17:32:57 +0400

    Создание GRUD на основе дженериков

--- modules/views.py
+++ modules/views.py
@@ -1,12 +1,40 @@
 from rest_framework.viewsets import ModelViewSet
-
-from modules.models import Module
+from rest_framework.generics import (CreateAPIView, DestroyAPIView,
+                                     ListAPIView, RetrieveAPIView,
+                                     UpdateAPIView)
+from modules.models import Module, Lesson
 
 from modules.pagination import ModulesPaginator
-from modules.serializer import ModuleSerializer
+from modules.serializer import ModuleSerializer, LessonSerializer
 
 
 class ModuleViewSet(ModelViewSet):
     serializer_class = ModuleSerializer
     queryset = Module.objects.all()
     pagination_class = ModulesPaginator
+
+
+class LessonCreateAPIView(CreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(ListAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+    pagination_class = ModulesPaginator
+
+
+class LessonRetrieveAPIView(RetrieveAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonUpdateAPIView(UpdateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDestroyAPIView(DestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
