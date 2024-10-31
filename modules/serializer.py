2f2c28a1fd133973aa051eb8e7069d820521471f
ystass <stepanischevyy@mail.ru>
2024-10-31 17:33:43 +0400

    Создание сериалайзера для Lesson

--- modules/serializer.py
+++ modules/serializer.py
@@ -1,6 +1,12 @@
 from rest_framework.serializers import ModelSerializer
 
-from modules.models import Module
+from modules.models import Module, Lesson
+
+
+class LessonSerializer(ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = "__all__"
 
 
 class ModuleSerializer(ModelSerializer):
