8cf3acb08f83924c7421b56cb96cde61a70e9a11
ystass <stepanischevyy@mail.ru>
2024-11-02 17:59:22 +0400

    Вывод информации об уроках модуля

--- modules/serializer.py
+++ modules/serializer.py
@@ -1,3 +1,4 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from modules.models import Lesson, Module
@@ -12,7 +13,12 @@ class LessonSerializer(ModelSerializer):
 
 
 class ModuleSerializer(ModelSerializer):
+    lessons_count = SerializerMethodField()
+    lessons = LessonSerializer(source="lesson_set", many=True, read_only=True)
 
     class Meta:
         model = Module
         fields = "__all__"
+
+    def get_lessons_count(self, instance):
+        return instance.lesson_set.count()
