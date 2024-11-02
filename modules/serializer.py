1b5696fd338170819cdeceb4976d88185dc4ad63
ystass <stepanischevyy@mail.ru>
2024-11-02 15:58:56 +0400

    Валидирование видео уроков

--- modules/serializer.py
+++ modules/serializer.py
@@ -1,12 +1,14 @@
 from rest_framework.serializers import ModelSerializer
 
 from modules.models import Lesson, Module
+from modules.validators import UrlValidator
 
 
 class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+        validators = [UrlValidator(field="url")]
 
 
 class ModuleSerializer(ModelSerializer):
