20eb19654ae5d9ffdb6d937264828d8282e53313
ystass <stepanischevyy@mail.ru>
2024-11-02 19:14:07 +0400

    Создание админок

--- modules/admin.py
+++ modules/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from modules.models import Module, Lesson
+
+
+@admin.register(Module)
+class UserCourse(admin.ModelAdmin):
+    list_display = ('id', 'serial_number', 'title', 'description', 'owner')
+
+
+@admin.register(Lesson)
+class UserLesson(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'module', 'owner')
