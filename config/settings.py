820d0141d569b4541f3feacd4008b72c43e3551f
ystass <stepanischevyy@mail.ru>
2024-11-04 20:54:12 +0400

    Закрываем эндпоинты для работы с модулями и уроками

--- config/settings.py
+++ config/settings.py
@@ -81,9 +81,9 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+      "rest_framework_simplejwt.authentication.JWTAuthentication",),
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
 }
 
 # Database
