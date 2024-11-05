b95eea755f916da6c787291ad38797a74492085a
ystass <stepanischevyy@mail.ru>
2024-11-05 10:40:44 +0400

    переменные окружения

--- config/settings.py
+++ config/settings.py
@@ -93,11 +93,11 @@ REST_FRAMEWORK = {
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "Educational_modules", #os.getenv("POSTGRES_DB"),
-        "USER": "postgres", #os.getenv("POSTGRES_USER"),
-        "PASSWORD": "qwerty", #os.getenv("POSTGRES_PASSWORD"),
-        # "HOST": os.getenv("POSTGRES_HOST"),
-        # "PORT": os.getenv("POSTGRES_PORT"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
