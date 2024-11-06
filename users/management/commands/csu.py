e22b8ef931d3f682c981f82643ebf94257fec57d
ystass <stepanischevyy@mail.ru>
2024-11-06 11:11:15 +0400

    переменные окружения

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -1,13 +1,13 @@
 from django.core.management import BaseCommand
 
 from users.models import User
-
+import os
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        user = User.objects.create(email="admin@admin.com",)
-        user.set_password("qwe123asd456")
+        user = User.objects.create(email=os.getenv("ADMIN_USER"),)
+        user.set_password(os.getenv("ADMIN_PASSWORD"))
         user.is_active = True
         user.is_staff = True
         user.is_superuser = True
